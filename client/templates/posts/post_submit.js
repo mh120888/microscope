9b085ad59cfc763b3cb9616d87b305697faf5c61
Matt Higgins <matthew.alan.higgins@gmail.com>
2016-02-13 15:17:11 -0800

    Validates post contents on submission

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -7,6 +7,12 @@ Template.postSubmit.events({
 			title: $(e.target).find('[name=title]').val()
 		}
 
+		var errors = validatePost(post);
+
+		if (Object.keys(errors).length > 0) {
+			return Session.set('postSubmitErrors', errors);
+		}
+
 		Meteor.call('postInsert', post, function(error, result) { // display the error to the user and abort
 			if (error)
 				return throwError(error.reason);
@@ -14,5 +20,18 @@ Template.postSubmit.events({
 				throwError('This link has already been posted. Here it is.');
 			Router.go('postPage', {_id: result._id});
 		});
+	}
+});
+
+Template.postSubmit.onCreated(function() {
+	Session.set('postSubmitErrors', {});
+});
+
+Template.postSubmit.helpers({
+	errorMessage: function(field) {
+		return Session.get('postSubmitErrors')[field];
+	},
+	errorClass: function(field) {
+		return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
 	}
 });
