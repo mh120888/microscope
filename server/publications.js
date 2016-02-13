ca2dbde6bce40e4a04ed088c2a6ade8531138bb1
unknown <Matthew.Higgins@MHIGGINS1.sd.local>
2016-02-12 16:46:42 -0800

    Adds allPosts publication

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,9 @@
 Meteor.publish('posts', function() {
     return Posts.find();
 });
+
+Meteor.publish('allPosts', function() {
+    return Posts.find({}, {fields: {
+        date: false
+    }});
+});
