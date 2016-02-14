98a1549f75c3c0386ca611db8be355a5000e25a0
Matt Higgins <matthew.alan.higgins@gmail.com>
2016-02-13 18:59:51 -0800

    Adds notifications

--- server/publications.js
+++ server/publications.js
@@ -2,13 +2,11 @@ Meteor.publish('posts', function() {
 	return Posts.find();
 });
 
-Meteor.publish('allPosts', function() {
-	return Posts.find({}, {fields: {
-		date: false
-	}});
-});
-
 Meteor.publish('comments', function(postId) {
 	check(postId, String);
 	return Comments.find({postId: postId});
+});
+
+Meteor.publish('notifications', function() {
+	return Notifications.find({userId: this.userId, read: false});
 });
